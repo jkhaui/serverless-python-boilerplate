caaa47f31f6360f91ae4b702878a114974f23660
jkhaui <jordan.lee.163@gmail.com>
2019-09-18 16:06:39 +1000

    Add warm-up plugin

--- handler.py
+++ handler.py
@@ -1,33 +1,49 @@
 import spacy
 import json
 
-# All Lambda layers are accessible within the /opt/ directory.
-nlp = spacy.load('/opt/en_core_web_sm-2.1.0')
-
-ERROR_MSG = 'Error: Please ensure that the `text` parameter is being passed'
+ERROR_MSG = 'Please ensure that the `text` parameter is being passed'
+EMPTY_MSG = 'No people were detected from the text provided.'
 
 
 def main(event, context):
-    def get_names(words):
-        doc = nlp(words)
-        names = []
+    if 'isOffline' in event:
+        # All Lambda layers are accessible within the /opt/ directory.
+        nlp = spacy.load('en_core_web_sm-2.1.0')
+    else:
+        nlp = spacy.load('/opt/en_core_web_sm-2.1.0')
+
+    # Return early to minimise resource usage if invoked by warmup plugin
+    if event.get('source') in ['aws.events', 'serverless-plugin-warmup']:
+        print('Keeping the Lambda warm...')
+        return {}
+
+    def get_names(text_):
+        doc = nlp(text_)
+        entities_ = []
 
         for ent in doc.ents:
             if ent.label_ == 'PERSON':
                 entity = {
                     'person': ent.text,
                 }
-                names.append(entity)
-        return names
+                entities_.append(entity)
+        return entities_
 
     try:
         text = event['queryStringParameters']['text']
-    except TypeError:
+    except (TypeError, KeyError):
         text = ERROR_MSG
 
-    entities = None
     if text is not ERROR_MSG:
         entities = get_names(text)
+        if not entities:
+            entities = [{
+                'empty': EMPTY_MSG
+            }]
+    else:
+        entities = [{
+            'error': ERROR_MSG
+        }]
 
     response = {
         'statusCode': 200,
